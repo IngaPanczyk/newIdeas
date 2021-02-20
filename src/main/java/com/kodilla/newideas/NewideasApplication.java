e5e8f3ffc054d6f2359082d5072626f9ad117b8b
IngaPanczyk <ingawaaa@gmail.com>
2021-02-20 10:17:26 +0100

    First Commit

---
+++ src/main/java/com/kodilla/newideas/NewideasApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.newideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewideasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewideasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/newideas/NewideasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.newideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewideasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
