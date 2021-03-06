9c99f4e6e16e6710af4534c26390e2ecc01ce593
IngaPanczyk <ingawaaa@gmail.com>
2021-03-06 14:29:26 +0100

    usunięcie jar

--- src/main/java/com/kodilla/newideas/NewideasApplication.java
+++ src/main/java/com/kodilla/newideas/NewideasApplication.java
@@ -3,6 +3,7 @@ package com.kodilla.newideas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 @Component
